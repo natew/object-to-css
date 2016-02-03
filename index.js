9bc4ad124afee11d779bbbaba9cfda3e92051bdc
natew <natewienert@gmail.com>
2016-02-02 21:30:02 -0800

    ive forgotten es5

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var addPx = require('add-px-to-style')
 var hyphenate = require('hyphenate-style-name')
 
 module.exports = function createMarkup(obj) {
-  let keys = Object.keys(obj)
+  var keys = Object.keys(obj)
   if (!keys.length) return ''
-  let i, len = keys.length
-  let result = ''
+  var i, len = keys.length
+  var result = ''
 
   for (i = 0; i < len; i++) {
-    let key = keys[i]
-    let val = obj[key]
+    var key = keys[i]
+    var val = obj[key]
     result += hyphenate(key) + ':' + addPx(key, val)
   }
 
